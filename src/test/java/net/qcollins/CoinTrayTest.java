1b90059111079b1f10d75bfe1cd2efb22e1b37f5
Quinn Collins <collinsqui@gmail.com>
2017-09-10 18:35:30 -0400

    added some missed tests for CoinTray class

---
+++ src/test/java/net/qcollins/CoinTrayTest.java
@@ -0,0 +1,23 @@
+package net.qcollins;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.qcollins.coinsystem.CoinTray;
+import net.qcollins.currency.Quarter;
+
+public class CoinTrayTest {
+	private CoinTray coinTray;
+	
+	@Before
+	public void setup() {
+		coinTray = new CoinTray();
+	}
+	
+	@Test
+	public void canAddCoinsToCoinTray() {
+		coinTray.addCoinToCoinTray(new Quarter());
+		Assert.assertEquals(1, coinTray.getCoinTray().size());
+	}
+}
