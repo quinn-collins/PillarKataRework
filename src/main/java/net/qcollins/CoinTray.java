3c66085cc3f67aec5175f85915037d51f87d5d99
Quinn Collins <collinsqui@gmail.com>
2017-09-10 17:20:06 -0400

    added CoinTray class with getter and setter

---
+++ src/main/java/net/qcollins/CoinTray.java
@@ -0,0 +1,22 @@
+package net.qcollins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.qcollins.currency.Currency;
+
+public class CoinTray {
+	private List<Currency> coinTray;
+	
+	public CoinTray() {
+		coinTray = new ArrayList<Currency>();
+	}
+	
+	public void addCoinToCoinTray(Currency coin) {
+		coinTray.add(coin);
+	}
+	
+	public List<Currency> getCoinTray() {
+		return coinTray;
+	}
+}
