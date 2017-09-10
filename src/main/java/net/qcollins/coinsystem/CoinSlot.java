2d1ca47a936d7d7d2d20958eac815f7b5a481099
Quinn Collins <collinsqui@gmail.com>
2017-09-10 19:40:29 -0400

    added tests and methods for returning change to customer

--- src/main/java/net/qcollins/coinsystem/CoinSlot.java
+++ src/main/java/net/qcollins/coinsystem/CoinSlot.java
@@ -2,15 +2,20 @@ package net.qcollins.coinsystem;
 
 import net.qcollins.DollarAmount;
 import net.qcollins.currency.Currency;
+import net.qcollins.currency.Dime;
+import net.qcollins.currency.Nickel;
+import net.qcollins.currency.Quarter;
 
 public class CoinSlot {
 	private CoinIdentifier coinIdentifier;
 	private CoinBank coinBank;
+	private CoinTray coinTray;
 	private DollarAmount currentBalance = DollarAmount.ZERO_DOLLARS;
 
 	public CoinSlot() {
 		coinIdentifier = new CoinIdentifier();;
 		coinBank = new CoinBank();
+		coinTray = new CoinTray();
 	}
 	
 	public CoinBank getCoinBank() {
@@ -25,5 +30,41 @@ public class CoinSlot {
 		Currency coin = coinIdentifier.identify(mass, diameter, thickness);
 		coinBank.addCoin(coin);
 		currentBalance = currentBalance.plus(coin.getValue());
+	}
+	
+	public void returnCoins() {
+		while(currentBalance.isGreaterThanOrEqualTo(DollarAmount.FIVE_CENTS)) {
+			if(currentBalance.isGreaterThanOrEqualTo(DollarAmount.TWENTY_FIVE_CENTS) && coinBank.getNumberOfQuarters() > 0) {
+				currentBalance = currentBalance.minus(DollarAmount.TWENTY_FIVE_CENTS);
+				for(Currency coin : coinBank.getCoinStock()) {
+					if(coin.getValue().getCents() == 25) {
+						coinBank.getCoinStock().remove(coin);
+						break;
+					}
+				}
+				coinTray.addCoinToCoinTray(new Quarter());
+				
+			}
+			else if(currentBalance.isGreaterThanOrEqualTo(DollarAmount.TEN_CENTS) && coinBank.getNumberOfDimes() > 0) {
+				currentBalance = currentBalance.minus(DollarAmount.TEN_CENTS);
+				for(Currency coin : coinBank.getCoinStock()) {
+					if(coin.getValue().getCents() == 10) {
+						coinBank.getCoinStock().remove(coin);
+						break;
+					}
+				}
+				coinTray.addCoinToCoinTray(new Dime());
+			}
+			else if(currentBalance.isGreaterThanOrEqualTo(DollarAmount.FIVE_CENTS) && coinBank.getNumberOfNickels() > 0) {
+				currentBalance = currentBalance.minus(DollarAmount.FIVE_CENTS);
+				for(Currency coin : coinBank.getCoinStock()) {
+					if(coin.getValue().getCents() == 5) {
+						coinBank.getCoinStock().remove(coin);
+						break;
+					}
+				}
+				coinTray.addCoinToCoinTray(new Nickel());
+			}
+		}
 	}
 }
--- src/test/java/net/qcollins/CoinSlotTest.java
+++ src/test/java/net/qcollins/CoinSlotTest.java
@@ -24,4 +24,13 @@ public class CoinSlotTest {
 	public void canGetCurrentBalance() {
 		Assert.assertEquals(25, coinSlot.getCurrentBalance().getCents());
 	}
+	
+	@Test
+	public void canReturnCoins() {
+		coinSlot.insertCoin(5, 21.21, 1.95);
+		coinSlot.insertCoin(2.268, 17.91, 1.35);
+		coinSlot.insertCoin(5.670, 24.26, 1.75);
+		coinSlot.returnCoins();
+		Assert.assertEquals(0, coinSlot.getCoinBank().getNumberOfQuarters());
+	}
 }
